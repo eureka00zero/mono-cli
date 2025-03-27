9e0cc055cd4379aa8b02ff08f1e5434bcbb5676d
cwang13 <cwang13@paypal.com>
2025-03-27 15:36:29 +0800

    init utils

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,5 +1,14 @@
+import { exec } from "child_process";
+import { input, select } from "@inquirer/prompts";
+
 async function create() {
   console.log("create 命令执行中...");
 }
 
-export default create;
+async function createComp(params: { name: string }) {
+  exec(`mkdir ${params.name}`);
+  exec(`cd ${params.name}`);
+  exec(`npm init -y`);
+}
+
+export { create, createComp };
---
+++ packages/utils/src/test.ts
@@ -0,0 +1,5 @@
+import { getNpmInfo } from "./versionutils.js";
+
+const info = getNpmInfo("create-vite");
+
+console.log(info);
